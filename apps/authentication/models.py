9e57c92890850e464a3969d802841ab64b4e46bd
Abdusamad <sevbofx@gmail.com>
2022-09-22 23:34:46 +0500

    `CustomUser` implemented

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,6 +1,23 @@
+import email
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from apps.authentication.managers import CustomUserManager
+
+class UserType(models.TextChoices):
+    ADMIN = 'admin', 'Admin'
+    TEACHER = 'teacher', 'Teacher'
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    user_type = models.CharField(choices=UserType.choices, max_length=10, default=UserType.TEACHER)
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
