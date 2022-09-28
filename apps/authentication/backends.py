9e0a9b0724f08a916c93122bf510c2a06fe1e4e2
Abdusamad <sevbofx@gmail.com>
2022-09-28 11:46:38 +0500

    `auth backends` changed to solve `can not login to admin panel` problem

--- apps/authentication/backends.py
+++ apps/authentication/backends.py
@@ -1,22 +1,17 @@
-from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
-from apps.authentication.models import User
+from django.contrib.auth.backends import ModelBackend
 
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, email=None, password=None, **kwargs):
+    def authenticate(self, request, **kwargs):
         UserModel = get_user_model()
         try:
+            email = kwargs.get('email', None)
+            if email is None:
+                email = kwargs.get('username', None)
             user = UserModel.objects.get(email=email)
+            if user.check_password(kwargs.get('password', None)):
+                return user
         except UserModel.DoesNotExist:
             return None
-        else:
-            if user.check_password(password):
-                return user
         return None
-    
-    def get_user(self, email):
-        try:
-            return User.objects.get(email=email)
-        except:
-            return None
