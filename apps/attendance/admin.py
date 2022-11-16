ce9d79cfd66eec6f0de809a2630c888debc1a26c
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-16 08:09:35 -0500

    `admin` subject implemented

--- apps/attendance/admin.py
+++ apps/attendance/admin.py
@@ -6,4 +6,18 @@ admin.site.register(Attendance)
 admin.site.register(AttendanceReport)
 admin.site.register(Group)
 admin.site.register(Student)
-admin.site.register(Subject)
+
+
+class M2MInline(admin.TabularInline):
+    model = Subject.group.through
+    extra = 1
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    inlines = [M2MInline]
+    list_display = ['name', 'slug', 'teacher']
+    list_filter = ['teacher']
+    search_fields = ['name', 'slug', 'teacher__first_name', 'teacher__last_name']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Subject, SubjectAdmin)
