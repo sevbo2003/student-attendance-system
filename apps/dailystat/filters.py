5a261989e2d78a025eb6d31fa1fad68823edf756
Abdusamad Malikov <sevbofx@gmail.com>
2022-11-18 11:48:31 -0500

    `filter by group` implemented

--- apps/dailystat/filters.py
+++ apps/dailystat/filters.py
@@ -8,13 +8,17 @@ from django.db.models import Q
 class DailyAttendanceStatFilter(filters.FilterSet):
     student = filters.CharFilter(method='filter_student')
     subjects = filters.CharFilter(method='filter_subjects_by_slug')
+    group = filters.CharFilter(method='filter_group')
 
     class Meta:
         model = DailyAttendanceStat
-        fields = ['student', 'subjects']
+        fields = ['student', 'subjects', 'group']
     
     def filter_student(self, queryset, name, value):
         return queryset.filter(Q(student__first_name__icontains=value) | Q(student__last_name__icontains=value))
     
     def filter_subjects_by_slug(self, queryset, name, value):
         return queryset.filter(subjects__slug=value)
+    
+    def filter_group(self, queryset, name, value):
+        return queryset.filter(student__group__name=value)
