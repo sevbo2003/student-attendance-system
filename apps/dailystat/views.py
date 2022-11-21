263768e9a2a2ff59c253b85bf5194ccf792d1c09
Abdusamad Malikov <sevbofx@gmail.com>
2022-11-21 15:06:46 -0500

    `Daily stat` view updated

--- apps/dailystat/views.py
+++ apps/dailystat/views.py
@@ -1,19 +1,22 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework import status
 from apps.dailystat.models import DailyAttendanceStat
 from apps.dailystat.serializers import DailyAttendanceStatSerializer
 from apps.dailystat.filters import DailyAttendanceStatFilter
-from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-import datetime
+from datetime import datetime
 
 
 class DailyAttendanceStatViewSet(viewsets.ModelViewSet):
-    queryset = DailyAttendanceStat.objects.filter(day=datetime.date.today()).order_by('student__last_name')
+    queryset = DailyAttendanceStat.objects.filter(day=datetime.today().date()).order_by('student__last_name')
     serializer_class = DailyAttendanceStatSerializer
     http_method_names = ['get', 'head', 'options']
     filterset_class = DailyAttendanceStatFilter
     filter_backends = [DjangoFilterBackend]
 
+    def get_queryset(self):
+            try:
+                queryset = DailyAttendanceStat.objects.filter(day=datetime.today().date()).order_by('student__last_name')
+            except:
+                last_day = DailyAttendanceStat.objects.first().day
+                queryset = DailyAttendanceStat.objects.filter(day=last_day).order_by('student__last_name')
+            return queryset
